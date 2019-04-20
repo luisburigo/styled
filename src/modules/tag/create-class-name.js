ffc556ccd97f403e0a05b65f3f2a96892d1e5971
guga <guga.burigo29@outlook.com>
2019-04-19 22:00:50 -0300

    add: tests for module create class names

--- src/modules/tag/create-class-name.js
+++ src/modules/tag/create-class-name.js
@@ -9,10 +9,49 @@ const cache = [];
 const allLetters = "ABCDDEFGHIJKLMNOPQRSTUVXZWYabcdefghijklmnopqrsuvxzwy";
 
 /**
+ * @description add clas name in cache
+ * @param {string} className
+ * @return {number}
+ */
+const addClassNameInCache = (className) => cache.push(className);
+
+/**
+ * @description return a boolean value if exist class in cache
+ * @param {string} className
+ * @return {boolean}
+ */
+export const hasClassInCache = (className) => cache.includes(className);
+
+/**
  * @description create random class name
  * @param {number} length
- * @return {String}
+ * @return {string}
  */
 export function createClassName(length) {
+    let name = randomName(length);
 
+    do {
+        name = randomName(length);
+    } while (hasClassInCache(name));
+
+    addClassNameInCache(name);
+
+    return name;
+}
+
+/**
+ * @description generate a random name
+ * @param {number} length
+ * @return {string}
+ */
+export function randomName(length) {
+    const lengthLetters = allLetters.length - 1;
+    const randomNumber = (length) => Math.round(Math.random() * length);
+    let name = '';
+
+    for (let i = 0; i < length; i++) {
+        name += allLetters[randomNumber(lengthLetters)];
+    }
+
+    return name;
 }
--- tests/tag/CreateClassTest.test.js
+++ tests/tag/CreateClassTest.test.js
@@ -1,5 +1,21 @@
-test('create a random class whit lenght 5', () => {
+import {createClassName, hasClassInCache, randomName} from "../../src/modules/tag/create-class-name";
 
+const tamanho = 10;
+const className = createClassName(tamanho);
 
+test(`create a random name with length ${tamanho}`, () => {
+    const name = randomName(tamanho);
+    expect(name).toHaveLength(tamanho);
+})
+;
 
+test(`create a random class name whit length ${tamanho}`, () => {
+    expect(className).toHaveLength(tamanho);
 });
+
+test('when generating the class, should be in the cache', () => {
+    const hasClass = hasClassInCache(className);
+    expect(hasClass).toEqual(true);
+})
+
+
