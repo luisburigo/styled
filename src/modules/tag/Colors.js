d45ab352ebb2165b5819548861cfd19711e4f01e
gugaburigo29 <guga.burigo29@outlook.com>
2019-05-09 21:38:09 -0300

    fix: alter to uppercase names of the colors

--- src/modules/tag/Colors.js
+++ src/modules/tag/Colors.js
@@ -1,10 +1,10 @@
 // All colors
 export const colors = {
-    red: '#ff0000',
-    green: '#00ff00',
-    blue: '#0000ff',
-    yellow: '#ffff00',
-    orange: '#ff5500'
+    RED: '#ff0000',
+    GREEN: '#00ff00',
+    BLUE: '#0000ff',
+    YELLOW: '#ffff00',
+    ORANGE: '#ff5500'
 };
 
 // Implementar conversão de cores para hexadecimal e rgba
